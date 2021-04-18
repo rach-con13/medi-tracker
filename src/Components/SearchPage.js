bd5971fd8f49f568375863ff53d6a3a7c1f2a0a8
Biprajit Majumder <biprajit163@gmail.com>
2021-04-18 13:02:56 -0400

    initial commit

---
+++ src/Components/SearchPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+function SearchPage(props) {
+    return (
+        <div>
+            <h1>Search page goes here!</h1>
+        </div>
+    );
+}
+
+
+export default SearchPage;
